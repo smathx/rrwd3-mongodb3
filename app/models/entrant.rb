899acb3254a477c9e896317e2fb058f5681656ad
Stephen Matthews <smath@opal7.fsnet.co.uk>
2016-02-04 17:34:27 +0000

    Implement Race / Results Cross-Collection passed

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -13,6 +13,12 @@ class Entrant
   embeds_many :results, as: :entrant, class_name: "LegResult",
     order: [:"event.o".asc], after_add: :update_total
   
+  embeds_one :race, class_name: "RaceRef"
+  
+  def the_race
+    race.race
+  end
+  
   def update_total(result)
     self.secs = self.secs.nil? ? result.secs: self.secs += result.secs
   end
--- app/models/race.rb
+++ app/models/race.rb
@@ -2,11 +2,14 @@ class Race
   include Mongoid::Document
   include Mongoid::Timestamps
   
-  embeds_many :events, as: :parent, order: [:order.asc]
-  
   field :n, as: :name, type: String
   field :date, type: Date
   field :loc, as: :location, type: Address
+  
+  embeds_many :events, as: :parent, order: [:order.asc]
+
+  has_many :entrants, foreign_key: "race._id", dependent: :delete,
+    order: [:secs.asc, :bib.asc]
   
   scope :upcoming, ->{ where(:date.gte => Date.current) }
   scope :past, ->{ where(:date.lt => Date.current) }
