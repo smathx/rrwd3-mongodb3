ff552092a2440afb075df8d0902a635438f6d0f3
Stephen Matthews <smath@opal7.fsnet.co.uk>
2016-02-04 16:48:42 +0000

    Implement Results Collection passed

---
+++ app/models/entrant.rb
@@ -0,0 +1,19 @@
+class Entrant
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  store_in collection: "results" 
+  
+  field :bib, type: Integer
+  field :secs, type: Float
+  field :o, as: :overall, type: Placing
+  field :gender, type: Placing
+  field :group, type: Placing
+  
+  embeds_many :results, as: :entrant, class_name: "LegResult",
+    order: [:"event.o".asc], after_add: :update_total
+  
+  def update_total(result)
+    self.secs = self.secs.nil? ? result.secs: self.secs += result.secs
+  end
+end
