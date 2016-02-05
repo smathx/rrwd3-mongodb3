5622bf6c4ce1beeb0baafd3ebe08815be7dfe7fc
Stephen Matthews <smath@opal7.fsnet.co.uk>
2016-02-05 22:55:54 +0000

    Set root to races#index - All done!

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     post "entries" => "racers#create_entry"
   end
   
+  root 'races#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
